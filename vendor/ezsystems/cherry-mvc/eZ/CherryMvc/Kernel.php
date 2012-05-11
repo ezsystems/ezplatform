e1bae322f0e7208cd6e2cc7b471ed4ab050c5f55
Jérôme Vieilledent <lolautruche@gmail.com>
2012-05-11 15:58:44 +0200

    Added base front controller + very basic kernel

---
+++ vendor/ezsystems/cherry-mvc/eZ/CherryMvc/Kernel.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * File containing the base Kernel class.
+ *
+ * @copyright Copyright (C) 1999-2012 eZ Systems AS. All rights reserved.
+ * @license http://www.gnu.org/licenses/gpl-2.0.txt GNU General Public License v2
+ * @version //autogentag//
+ */
+
+namespace eZ\CherryMvc;
+use Symfony\Component\HttpKernel\HttpKernel;
+
+class Kernel extends HttpKernel
+{
+
+}
--- web/index.php
+++ web/index.php
@@ -1,1 +1,30 @@
-Hello world !
+<?php
+require_once __DIR__.'/../vendor/autoload.php';
+
+use eZ\CherryMvc\Kernel;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing;
+use Symfony\Component\HttpKernel;
+use Symfony\Component\EventDispatcher\EventDispatcher;
+
+$routes = include __DIR__.'/../src/routes.php';
+
+// Routes will be handled by the RouterListener, which interacts with kernel.request event
+$dispatcher = new EventDispatcher();
+$dispatcher->addSubscriber(
+    new HttpKernel\EventListener\RouterListener(
+        new Routing\Matcher\UrlMatcher(
+            $routes,
+            new Routing\RequestContext()
+        )
+    )
+);
+
+$kernel = new Kernel(
+    $dispatcher,
+    new HttpKernel\Controller\ControllerResolver()
+);
+
+$response = $kernel->handle( Request::createFromGlobals() );
+$response->send();
