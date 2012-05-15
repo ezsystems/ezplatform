7113247c0ba0ae2335723246a1d357eb3b46fab9
Jérôme Vieilledent <lolautruche@gmail.com>
2012-05-15 18:46:33 +0200

    Introducing more complete kernel, with bundle system

---
+++ app/EzPublishKernel.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * File containing the EzPublishKernel class.
+ *
+ * @copyright Copyright (C) 1999-2012 eZ Systems AS. All rights reserved.
+ * @license http://www.gnu.org/licenses/gpl-2.0.txt GNU General Public License v2
+ * @version //autogentag//
+ */
+
+use eZ\Bundle\EzPublishCoreBundle\EzPublishCoreBundle;
+use Symfony\Component\HttpKernel\Kernel;
+use Symfony\Component\Config\Loader\LoaderInterface;
+
+class EzPublishKernel extends Kernel
+{
+    /**
+     * Returns an array of bundles to registers.
+     *
+     * @return array An array of bundle instances.
+     *
+     * @api
+     */
+    public function registerBundles()
+    {
+        return array(
+            new EzPublishCoreBundle()
+        );
+    }
+
+    /**
+     * Loads the container configuration
+     *
+     * @param LoaderInterface $loader A LoaderInterface instance
+     *
+     * @api
+     */
+    public function registerContainerConfiguration( LoaderInterface $loader )
+    {
+        $loader->load(__DIR__.'/config/config_'.$this->getEnvironment().'.yml');
+    }
+}
--- web/index.php
+++ web/index.php
@@ -1,44 +1,10 @@
 <?php
 require_once __DIR__.'/../vendor/autoload.php';
+require_once __DIR__ . '/../app/EzPublishKernel.php';
 
-use eZ\CherryMvc\Kernel;
-use eZ\CherryMvc\Controller\Resolver;
-use eZ\CherryMvc\Template\Factory as TemplateFactory;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing;
-use Symfony\Component\HttpKernel\EventListener\RouterListener;
-use Symfony\Component\EventDispatcher\EventDispatcher;
-use \Twig_Loader_String;
-use \Twig_Environment;
 
-$routes = include __DIR__.'/../src/routes.php';
-
-// Routes will be handled by the RouterListener, which interacts with kernel.request event
-$dispatcher = new EventDispatcher();
-$dispatcher->addSubscriber(
-    new RouterListener(
-        new Routing\Matcher\UrlMatcher(
-            $routes,
-            new Routing\RequestContext(),
-            $dispatcher
-        )
-    )
-);
-
-$templateFactory = new TemplateFactory();
-$templateFactory->register(
-    "twig",
-    function ()
-    {
-        return new Twig_Environment( new Twig_Loader_String() );
-    }
-);
-
-$kernel = new Kernel(
-    $dispatcher,
-    new Resolver( $templateFactory )
-);
-
+$kernel = new EzPublishKernel( 'dev', true );
+$kernel->loadClassCache();
 $response = $kernel->handle( Request::createFromGlobals() );
 $response->send();
