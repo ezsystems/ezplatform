e244615c778aa9fec87e442f06b8663b185b9ae9
Jérôme Vieilledent <lolautruche@gmail.com>
2012-07-28 00:30:13 +0200

    Added a DIC extension in eZDemoBundle, adding a basic ContentViewProvider and a PreContentViewListener

---
+++ src/EzSystems/DemoBundle/DependencyInjection/eZDemoExtension.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * File containing the eZDemoExtension class.
+ *
+ * @copyright Copyright (C) 1999-2012 eZ Systems AS. All rights reserved.
+ * @license http://www.gnu.org/licenses/gpl-2.0.txt GNU General Public License v2
+ * @version //autogentag//
+ */
+
+namespace EzSystems\DemoBundle\DependencyInjection;
+
+use Symfony\Component\HttpKernel\DependencyInjection\Extension,
+    Symfony\Component\DependencyInjection\ContainerBuilder,
+    Symfony\Component\DependencyInjection\Loader\YamlFileLoader,
+    Symfony\Component\Config\FileLocator;
+
+class eZDemoExtension extends Extension
+{
+    /**
+     * Loads a specific configuration.
+     *
+     * @param array            $config    An array of configuration values
+     * @param ContainerBuilder $container A ContainerBuilder instance
+     *
+     * @throws InvalidArgumentException When provided tag is not defined in this extension
+     *
+     * @api
+     */
+    public function load( array $config, ContainerBuilder $container )
+    {
+        $loader = new YamlFileLoader(
+            $container,
+            new FileLocator( __DIR__ . '/../Resources/config' )
+        );
+
+        // Base services override
+        $loader->load( 'services.yml' );
+    }
+}
