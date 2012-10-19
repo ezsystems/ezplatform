78303060c878ed107f89565ee67358d21f06d113
Jérôme Vieilledent <lolautruche@gmail.com>
2012-10-19 07:25:33 +0200

    Removed unused helloWorld actions

--- src/EzSystems/DemoBundle/Controller/DemoController.php
+++ src/EzSystems/DemoBundle/Controller/DemoController.php
@@ -42,62 +42,6 @@ class DemoController extends Controller
         );
     }
 
-    public function helloWorldAction()
-    {
-        $response = new Response( "Hello World!" );
-        return $response;
-    }
-
-    public function helloWorldCachedAction()
-    {
-        $response = new Response();
-        $response->setPublic();
-        $response->setETag( "HelloWorldTag" );
-        $response->setLastModified( new DateTime( "2012-01-01 00:00:00+0000" ) );
-
-        // Check that the Response is not modified for the given Request
-        if ( $response->isNotModified( $this->getRequest() ) )
-        {
-            // return the 304 Response immediately
-            return $response;
-        }
-        $response->setContent( "Hello Universe!" );
-
-        return $response;
-    }
-
-    public function helloWorldTwigAction()
-    {
-        return $this->render( "eZDemoBundle::hello_world.html.twig" );
-    }
-
-    public function helloWorldTwigCachedAction()
-    {
-        $response = new Response();
-        $response->setPublic();
-        $response->setETag( "HelloWorldTwigTag" );
-//        $response->setLastModified( new DateTime( "2012-01-01 00:00:00+0000" ) );
-
-        // Check that the Response is not modified for the given Request
-        if ( $response->isNotModified( $this->getRequest() ) )
-        {
-            // return the 304 Response immediately
-            return $response;
-        }
-
-        return $this->render( "eZDemoBundle::hello_world.html.twig", array(), $response );
-    }
-
-    public function editorialAction( $contentId )
-    {
-        return $this->render(
-            "eZDemoBundle:content:editorial.html.twig",
-            array(
-                 "content" => $this->getRepository()->getContentService()->loadContent( $contentId )
-            )
-        );
-    }
-
     /**
      * Renders the top menu, with cache control
      *
