75851879b97bf14bff78734605df2c7eaa4999dd
Jérôme Vieilledent <lolautruche@gmail.com>
2012-11-09 14:32:44 +0100

    Removed EzPublishCache::getOptions() since it provided same options as default

--- ezpublish/EzPublishCache.php
+++ ezpublish/EzPublishCache.php
@@ -11,16 +11,4 @@ use eZ\Bundle\EzPublishCoreBundle\HttpCache;
 
 class EzPublishCache extends HttpCache
 {
-    protected function getOptions()
-    {
-        return array(
-            "debug" => false,
-            "default_ttl" => 0,
-            "private_headers" => array( "Authorization", "Cookie" ),
-            "allow_reload" => false,
-            "allow_revalidate" => false,
-            "stale_while_revalidate" => 2,
-            "stale_if_error" => 60,
-        );
-    }
 }
