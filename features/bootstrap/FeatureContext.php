f4b626eba68d56a74859d15fc6d0f4ea71ae49fe
Tobias Schlitt <toby@php.net>
2013-03-13 14:55:56 +0100

    Implemented: Hacked a first real Behat test case.

--- features/bootstrap/FeatureContext.php
+++ features/bootstrap/FeatureContext.php
@@ -18,7 +18,7 @@ use Behat\MinkExtension\Context\MinkContext;
 /**
  * Features context.
  */
-class FeatureContext extends BehatContext
+class FeatureContext extends MinkContext
 {
     /**
      * Initializes context.
@@ -28,8 +28,57 @@ class FeatureContext extends BehatContext
      */
     public function __construct(array $parameters)
     {
-        $this->useContext( 'mink', new MinkContext );
     }
+
+    /**
+     * @When /^I search for "([^"]*)"$/
+     */
+    public function iSearchFor($searchPhrase)
+    {
+        $searchField = $this->getSession()->getPage()->findById('site-wide-search-field');
+
+        $searchField->setValue($searchPhrase);
+        $this->getSession()->executeScript("$('#site-wide-search').submit();");
+    }
+
+    /**
+     * @Then /^I am on the "([^"]*)"$/
+     */
+    public function iAmOnThe($pageIdentifier)
+    {
+        // FIXME: Replace with waiting for the page to load
+        $this->getSession()->wait(5000);
+
+        // FIXME: Sanitize URLs in a central place
+        $currentFullUrl = $this->getSession()->getCurrentUrl();
+        $currentUrl = substr($currentFullUrl, 0, strpos($currentFullUrl, '?'));
+
+        $expectedUrl = $this->locatePath('/content/search');
+
+        // TODO: Use assertions
+        if ($currentUrl !== $expectedUrl) {
+            throw new \RuntimeException("Incorrect URL: '{$currentUrl}'. Expected: '{$expectedUrl}'");
+        }
+    }
+
+    /**
+     * @Given /^I see search (\d+) result$/
+     */
+    public function iSeeSearchResults($arg1)
+    {
+        $resultCountElement = $this->getSession()->getPage()->find('css', 'div.feedback');
+
+        // TODO: Use assertions
+        if ($resultCountElement === null) {
+            throw new \RuntimeException("Could not find text with number of search results.");
+        }
+
+        $resultText = $resultCountElement->getText();
+        if ($resultText !== 'Search for "welcome" returned 1 matches') {
+            throw new \RuntimeException("Result text '{$resultText}' did not match.");
+        }
+    }
+
 
 //
 // Place your definition and hook methods here:
--- features/start_page.feature
+++ features/start_page.feature
@@ -4,3 +4,11 @@ Feature: Start page
     Scenario: Start page displays welcome text
         Given I am on the homepage
         Then I should see "Welcome to eZ Publish"
+
+    @javascript
+    Scenario: Search works from the start page
+        Given I am on the homepage
+         When I search for "welcome"
+         Then I am on the "Search Page"
+          And I see search 1 result
+
