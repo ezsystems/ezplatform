62bd12769a2cccd994d9863b4316dbdbea4885e5
Jérôme Vieilledent <lolautruche@gmail.com>
2013-04-24 11:50:06 +0200

    Updated comments in index.php

--- web/index.php
+++ web/index.php
@@ -8,8 +8,6 @@ $loader = require_once __DIR__ . '/../ezpublish/autoload.php';
 // Use APC for autoloading to improve performance:
 // Change 'ezpublish' to a unique prefix in order to prevent cache key conflicts
 // with other applications also using APC.
-//
-// ( Not needed when using `php composer.phar dump-autoload --optimize` )
 /*
 $loader = new ApcClassLoader( 'ezpublish', $loader );
 $loader->register( true );
@@ -20,6 +18,7 @@ require_once __DIR__ . '/../ezpublish/EzPublishCache.php';
 
 $kernel = new EzPublishKernel( 'prod', false );
 $kernel->loadClassCache();
+// Comment the following line if you use an external reverse proxy (e.g. Varnish)
 $kernel = new EzPublishCache( $kernel );
 $request = Request::createFromGlobals();
 // Uncomment the following if your application is behind a reverse proxy you manage and trust.
