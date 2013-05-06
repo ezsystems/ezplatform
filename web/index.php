dc2ffb23b7e859c38cf7241489d861cc39bf4e56
Jérôme Vieilledent <lolautruche@gmail.com>
2013-05-06 11:41:23 +0200

    Removed comment in index.php leading to use depreacted Request::trustProxyData()

--- web/index.php
+++ web/index.php
@@ -21,9 +21,8 @@ $kernel->loadClassCache();
 // Comment the following line if you use an external reverse proxy (e.g. Varnish)
 $kernel = new EzPublishCache( $kernel );
 $request = Request::createFromGlobals();
-// Uncomment the following if your application is behind a reverse proxy you manage and trust.
-// (see http://fabien.potencier.org/article/51/create-your-own-framework-on-top-of-the-symfony2-components-part-2)
-//Request::trustProxyData();
+// If you are behind a trusted reverse proxy, you might set it as "trusted" in order to get correct client IP
+// See HttpFoundation\Request::setTrustedProxies()
 $response = $kernel->handle( $request );
 $response->send();
 $kernel->terminate( $request, $response );
