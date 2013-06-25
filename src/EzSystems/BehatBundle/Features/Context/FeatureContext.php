aea08a1ecb5354bdb2c53e023cacd4ac03aa7884
Patrick Allaert <pa@ez.no>
2013-06-25 13:23:32 +0200

    Added: missing copyright headers

--- src/EzSystems/BehatBundle/Features/Context/FeatureContext.php
+++ src/EzSystems/BehatBundle/Features/Context/FeatureContext.php
@@ -1,4 +1,13 @@
 <?php
+/**
+ * File containing the FeatureContext class.
+ *
+ * This class contains general feature context for Behat.
+ *
+ * @copyright Copyright (C) 1999-2013 eZ Systems AS. All rights reserved.
+ * @license http://www.gnu.org/licenses/gpl-2.0.txt GNU General Public License v2
+ * @version //autogentag//
+ */
 
 namespace EzSystems\BehatBundle\Features\Context;
 
