61c400c7de32313b6ef818527a6679c4fd1a7c77
MLoureiro <mloureiro.dev@gmail.com>
2014-04-15 12:02:33 +0100

    BDD: update single identifier sentences with ""

--- src/EzSystems/BehatBundle/Features/Context/SentencesInterfaces/ContentTypeGroup.php
+++ src/EzSystems/BehatBundle/Features/Context/SentencesInterfaces/ContentTypeGroup.php
@@ -19,31 +19,31 @@ use Behat\Gherkin\Node\TableNode;
 interface ContentTypeGroup
 {
     /**
-     * @Given /^I have (?:a |)Content Type Group (?P<identifier>[A-Z])$/
+     * @Given /^I have (?:a |)Content Type Group "(?P<identifier>[A-Z])"$/
      * @Given /^I have (?:a |)Content Type Group with "(?P<identifier>[^"])" identifier$/
      */
     public function iHaveContentTypeGroup( $identifier );
 
     /**
-     * @Given /^I (?:do not|don\'t) have a Content Type Group (?P<identifier>[A-Z])$/
+     * @Given /^I (?:do not|don\'t) have a Content Type Group "(?P<identifier>[A-Z])"$/
      * @Given /^I (?:do not|don\'t) have a Content Type Group with "(?P<identifier>[^"])" identifier$/
      */
     public function iDonTHaveContentTypeGroup( $identifier );
 
     /**
-     * @When /^I create a Content Type Group (?P<identifier>[A-Z])$/
+     * @When /^I create a Content Type Group "(?P<identifier>[A-Z])"$/
      * @When /^I create a Content Type Group with "(?P<identifier>[^"])" identifier$/
      */
     public function iCreateContentTypeGroup( $identifier );
 
     /**
-     * @Then /^I see a Content Type Group (?P<identifier>[A-Z])$/
+     * @Then /^I see a Content Type Group "(?P<identifier>[A-Z])"$/
      * @Then /^I see a Content Type Group with "(?P<identifier>[^"])" identifier$/
      */
     public function iSeeContentTypeGroup( $identifier );
 
     /**
-     * @Then /^I see (?P<total>\d+) Content Type Group(?:s|) (?P<identifier>[A-Z])$/
+     * @Then /^I see (?P<total>\d+) Content Type Group(?:s|) "(?P<identifier>[A-Z])"$/
      * @Then /^I see (?P<total>\d+) Content Type Group(?:s|) with "(?P<identifier>[^"])" identifier$/
      */
     public function iSeeTotalContentTypeGroup( $total, $identifier );
