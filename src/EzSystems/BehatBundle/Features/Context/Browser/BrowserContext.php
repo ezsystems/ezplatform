1f4ab8a3cfd0673b03bc8cc44df702c79aeefb88
André R <ar@ez.no>
2014-04-15 14:57:39 +0200

    [Behat] Add temp class alias to be able to get tests running

---
+++ src/EzSystems/BehatBundle/Features/Context/Browser/BrowserContext.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * File containing the BrowserContext class.
+ *
+ * This class contains general feature context for Behat.
+ *
+ * @copyright Copyright (C) 1999-2014 eZ Systems AS. All rights reserved.
+ * @license http://www.gnu.org/licenses/gpl-2.0.txt GNU General Public License v2
+ * @version //autogentag//
+ */
+
+namespace EzSystems\BehatBundle\Features\Context\Browser;
+
+use EzSystems\BehatBundle\Features\Context\BrowserContext as BaseFeatureContext;
+
+/**
+ * TEMP class to be able to boot test
+ */
+class BrowserContext extends BaseFeatureContext
+{
+}
