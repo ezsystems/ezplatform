a1e1082f9b6ec2bbc1d5804e33a2a85a313be6a8
André R <ar@ez.no>
2014-05-11 17:51:30 +0200

    [Travis] cleanup how fast cgi is setup
    Tries to make the setup easier to read and closer to how travis
    documents setting up apache + php + fastcgi.
    
    Conflicts:
    	bin/.travis/apache2/php5-fcgi
    	doc/conf/httpd/apache2/vhost.fastcgi.dev.template
    	doc/conf/httpd/apache2/vhost.fastcgi.prod.template

--- bin/.travis/configure_apache2.sh
+++ bin/.travis/configure_apache2.sh
@@ -5,24 +5,19 @@ sed s?%basedir%?$TRAVIS_BUILD_DIR? bin/.travis/apache2/behat_vhost | sudo tee /e
 sudo cp bin/.travis/apache2/php5-fcgi /etc/apache2/conf.d/php5-fcgi
 
 # modules enabling
-sudo a2enmod rewrite
-sudo a2enmod actions
-sudo a2enmod fastcgi
+sudo a2enmod rewrite actions fastcgi alias
 
 # sites disabling & enabling
 sudo a2dissite default
 sudo a2ensite behat
 
 # FPM
-DIR=$(dirname "$0")
-PHP_FPM_BIN="$HOME/.phpenv/versions/$TRAVIS_PHP_VERSION/sbin/php-fpm"
-PHP_FPM_CONF="$DIR/php-fpm.conf"
 USER=$(whoami)
 
-echo "
+sudo echo "
 [global]
 
-[travis]
+[www]
 user = $USER
 group = $USER
 listen = /tmp/php-fpm.sock
@@ -30,10 +25,11 @@ pm = static
 pm.max_children = 2
 
 php_admin_value[memory_limit] = 256M
-" > $PHP_FPM_CONF
+" > ~/.phpenv/versions/$(phpenv version-name)/etc/php-fpm.conf
 
-echo 'date.timezone = "Europe/Oslo"' >> ~/.phpenv/versions/$TRAVIS_PHP_VERSION/etc/conf.d/travis.ini
+sudo echo 'date.timezone = "Europe/Oslo"' >> ~/.phpenv/versions/$TRAVIS_PHP_VERSION/etc/conf.d/travis.ini
+sudo echo "cgi.fix_pathinfo = 1" >> ~/.phpenv/versions/$(phpenv version-name)/etc/php.ini
 
 # restart
-sudo $PHP_FPM_BIN --fpm-config "$PHP_FPM_CONF"
+sudo ~/.phpenv/versions/$(phpenv version-name)/sbin/php-fpm
 sudo service apache2 restart
