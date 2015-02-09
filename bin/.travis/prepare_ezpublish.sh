533a56ca72256ca3b740c6310c26eec4b4bc701d
Bertrand Dunogier <bd@ez.no>
2015-02-09 19:33:11 +0100

    Revert "EZP-23934: Made tests 'install legacy'"
    This reverts commit 39ab9787be4d20f537afbf6c8cb1671bb4e912de.

--- bin/.travis/prepare_ezpublish.sh
+++ bin/.travis/prepare_ezpublish.sh
@@ -5,43 +5,6 @@
 echo "> Setup github auth key to not reach api limit"
 ./bin/.travis/install_composer_github_key.sh
 
-echo "> Add legacy-bridge to requirements"
-composer require --no-update "ezsystems/legacy-bridge:dev-master"
-
-echo "> Configuring legacy"
-php ./bin/.travis/enablelegacybundle.php
-
-## add legacy routes
-cat << EOF >> ezpublish/config/routing.yml
-_ezpublishLegacyRoutes:
-    resource: @EzPublishLegacyBundle/Resources/config/routing.yml
-EOF
-
-## add setup wizard security rule
-cat << EOF >> ezpublish/config/security.yml
-        ezpublish_setup:
-            pattern: ^/ezsetup
-            security: false
-
-EOF
-
-## enable legacy mode on admin siteaccess
-cat << EOF >> ezpublish/config/ezpublish_behat.yml
-ez_publish_legacy:
-    system:
-        behat_site:
-            legacy_mode: false
-        behat_site_admin:
-            legacy_mode: true
-
-EOF
-
-## add legacy post-*-cmd scripts
-php ./bin/.travis/add_legacy_composer_scripts.php
-
-## enable legacy template engine
-sed -i "s/engines: \['twig/engines: ['eztpl', 'twig/" ezpublish/config/config.yml
-
 echo "> Install dependencies through composer"
 composer install --dev --prefer-dist
 
