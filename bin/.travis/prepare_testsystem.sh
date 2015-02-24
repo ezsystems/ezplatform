82da7112b296c833ee0677c3a537b60fe1d2fd57
André R <ar@ez.no>
2015-02-24 23:32:46 +0100

    Add script to be able to run behat tests from other repos

--- bin/.travis/prepare_testsystem.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-# This file is a alias for test system configured in behat.yml
-./bin/.travis/prepare_sahi.sh
---
+++ bin/.travis/setup_from_external_repo.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# This script is meant to be reused from other repos that needs to run behat tests.
+#
+# Example use:
+#
+# before_install:
+#  - git fetch --unshallow && git checkout -b tmp_travis_branch
+#  - export BRANCH_BUILD_DIR=$TRAVIS_BUILD_DIR
+#  - export TRAVIS_BUILD_DIR="$HOME/build/ezpublish-community"
+#  - cd "$HOME/build"
+#  - git clone --depth 1 --single-branch --branch master https://github.com/ezsystems/ezpublish-community.git
+#  - cd ezpublish-community
+#  - ./bin/.travis/setup_from_external_repo.sh $BRANCH_BUILD_DIR "ezsystems/demobundle:dev-tmp_travis_branch"
+
+REPO_DIR=$1
+COMPOSER_REQUIRE=$2
+
+./bin/.travis/prepare_system.sh
+./bin/.travis/prepare_sahi.sh
+
+echo "> Modify composer.json to point to local checkout"
+sed -i '$d' composer.json
+echo ',    "repositories": [{"type":"git","url":"'$REPO_DIR'"}]}' >> composer.json
+composer require --no-update "$COMPOSER_REQUIRE"
+cat composer.json
+
+./bin/.travis/prepare_ezpublish.sh
