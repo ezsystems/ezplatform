1ae0f90857f6ff115752e19e9d726d6378076723
André R <ar@ez.no>
2015-04-10 16:13:21 +0200

    [Travis] DEBUGGING -> DEBUG

--- bin/.travis/prepare_system.sh
+++ bin/.travis/prepare_system.sh
@@ -9,7 +9,7 @@
 ##       https://github.com/facebook/hiphop-php/commit/0b2dfdf4492eb06a125b068e939d092ec0588e5c
 
 # Disable xdebug to speed things up
-if [ "$DEBUGGING" == "" -a "$TRAVIS_PHP_VERSION" != "" -a "$TRAVIS_PHP_VERSION" != "hhvm" ] ; then
+if [ "$DEBUG" == "" -a "$TRAVIS_PHP_VERSION" != "" -a "$TRAVIS_PHP_VERSION" != "hhvm" ] ; then
     phpenv config-rm xdebug.ini ;
 fi
 
