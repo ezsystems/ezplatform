8430ec34df8a0464c2ccac53c09bccdf8bd937b0
Joao Inacio <joao.inacio@ez.no>
2015-10-29 16:37:16 +0000

    update/fix travis, behat env configs

--- bin/.travis/prepare_selenium2.sh
+++ bin/.travis/prepare_selenium2.sh
@@ -3,8 +3,8 @@
 echo "> Prepare X and start Selenium"
 export DISPLAY=:99.0
 sh -e /etc/init.d/xvfb start
-wget http://selenium-release.storage.googleapis.com/2.47/selenium-server-standalone-2.47.1.jar
-java -jar selenium-server-standalone-2.47.1.jar > /dev/null 2>&1 &
+wget http://selenium-release.storage.googleapis.com/2.48/selenium-server-standalone-2.48.2.jar
+java -jar selenium-server-standalone-2.48.2.jar > /dev/null 2>&1 &
 cd -
 
 # Give Selenium some time to start, otherwise tests will fail under high load on test servers
