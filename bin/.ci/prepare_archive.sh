277b21fb6ab2761b62c96854e9b6c0f9485c0965
André R <ar@ez.no>
2015-10-30 14:24:48 -0700

    [CI] Fix inline doc naming in prepare_archive.sh

--- bin/.ci/prepare_archive.sh
+++ bin/.ci/prepare_archive.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Script used to prepare for eZ Publish/Platform archives
+# Script used to prepare for eZ Platform/Studio archives
 #
 # Pre-requirement for LTS (EE) archives:
 #     auth.json needs to be placed in either root directory or COMPOSER_HOME.
