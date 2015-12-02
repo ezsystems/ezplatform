804f962c2a97459c17e0701fcc9d36f36a0e5c38
Edi Modrić <emodric@users.noreply.github.com>
2015-12-02 14:53:36 +0100

    EZP-25100: Fix class name in AppKernel.php after #70

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * File containing the EzPublishKernel class.
+ * File containing the AppKernel class.
  *
  * @copyright Copyright (C) eZ Systems AS. All rights reserved.
  * @license For full copyright and license information view LICENSE file distributed with this source code.
