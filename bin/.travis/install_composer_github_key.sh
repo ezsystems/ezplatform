ff397de39798da7e543dee7dfcf127d24a3a81ea
André R <ar@ez.no>
2016-01-06 14:43:32 +0100

    [Travis] Update approach for Github token to same as Symfony uses
    This approach, *as used by [Symfony](https://github.com/symfony/symfony/commit/6215437bdd6d6fc67ee167aea5e88324117be668) now*, is more straight forward, and avoids setting a bad example for others *(risking they create tokens with a scope without bein protected by Github's commit token scanners, unintentionally exposing rights to their repos)*.
    
    Besides this moves us one micro step towards enabling appveyor use (Continuous integration on Windows).

--- bin/.travis/install_composer_github_key.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-# This is unfortunately only way to avoid Github API limits with Travis on an open source project.
-# Please don't use our token, neither token nor it's user gives access to anything other then authentication, so better if you create your own.
-# PS: The simple obfuscation here is only to avoid Github from detecting this on commits.
-export EZ_GITHUB_TOKEN_A=`echo "574503f4468c5de5820d" | rev`
-export EZ_GITHUB_TOKEN_B=`echo "90cf1c134798de2dae27" | rev`
-
-composer config -g github-oauth.github.com "${EZ_GITHUB_TOKEN_A}${EZ_GITHUB_TOKEN_B}"
