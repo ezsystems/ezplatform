eb71433a05855fc7797b71301e794e5748e4e480
Damien Pobel <dp@ez.no>
2016-03-07 09:45:04 +0100

    Change selenium command to log its output

--- bin/.travis/prepare_selenium2.sh
+++ bin/.travis/prepare_selenium2.sh
@@ -4,7 +4,7 @@ echo "> Prepare X and start Selenium"
 export DISPLAY=:99.0
 sh -e /etc/init.d/xvfb start
 wget http://selenium-release.storage.googleapis.com/2.47/selenium-server-standalone-2.47.1.jar
-java -jar selenium-server-standalone-2.47.1.jar > /dev/null 2>&1 &
+java -jar selenium-server-standalone-2.47.1.jar -log /tmp/selenium.log &
 cd -
 
 # Give Selenium some time to start, otherwise tests will fail under high load on test servers
