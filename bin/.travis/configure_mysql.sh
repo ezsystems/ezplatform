082b900108bfcd9fddcd04868bc44ecf5dbb5cbc
Damien Pobel <dp@ez.no>
2016-03-08 14:15:15 +0100

    Make sure to create the database with the utf8 character set

--- bin/.travis/configure_mysql.sh
+++ bin/.travis/configure_mysql.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 
 echo "> Create database and grant premissions to user 'ezp'"
-mysql -uroot -e "CREATE DATABASE IF NOT EXISTS behattestdb; GRANT ALL ON behattestdb.* TO ezp@localhost IDENTIFIED BY 'ezp';"
+mysql -uroot -e "CREATE DATABASE IF NOT EXISTS behattestdb CHARACTER SET utf8; GRANT ALL ON behattestdb.* TO ezp@localhost IDENTIFIED BY 'ezp';"
