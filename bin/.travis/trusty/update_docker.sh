0342aaad30865dd824c2356f5198416de5c83c46
André R <ar@ez.no>
2016-08-09 23:29:41 +0200

    [Travis] Skip updating Docker as Travis now comes with v1.12.0

--- bin/.travis/trusty/update_docker.sh
+++ bin/.travis/trusty/update_docker.sh
@@ -1,18 +1,19 @@
 #!/usr/bin/env sh
 
 # Update package info and selectively update docker-engine (and keep old travis specific config file)
-sudo apt-get update
-sudo apt-get --reinstall -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" install docker-engine
-docker -v
+#disabled to save startup time as last travis image already have docker v1.12.0
+#sudo apt-get update
+#sudo apt-get --reinstall -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" install docker-engine
+#docker -v
 
 # If we need to pin it to a given version:
 # sudo apt-get --reinstall -y [...] install docker-engine=1.11.0-0~jessie
 # http://apt.dockerproject.org/repo/dists/debian-jessie/main/binary-amd64/Packages
 
 
-DOCKER_COMPOSE_VERSION="1.7.1"
+DOCKER_COMPOSE_VERSION="1.8.0"
 echo "\nUpdating Docker Compose to ${DOCKER_COMPOSE_VERSION}"
-sudo rm /usr/local/bin/docker-compose
+sudo rm -f /usr/local/bin/docker-compose
 curl -L https://github.com/docker/compose/releases/download/${DOCKER_COMPOSE_VERSION}/docker-compose-`uname -s`-`uname -m` > docker-compose
 chmod +x docker-compose
 sudo mv docker-compose /usr/local/bin
