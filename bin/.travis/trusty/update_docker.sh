8622e5c72ea7b5c2192ad2dc29eb25196476fb85
André R <ar@ez.no>
2016-11-12 14:21:29 +0100

    [Travis] Skip updating Docker Compose, it's already at newer version now on vm

--- bin/.travis/trusty/update_docker.sh
+++ bin/.travis/trusty/update_docker.sh
@@ -11,9 +11,9 @@
 # http://apt.dockerproject.org/repo/dists/debian-jessie/main/binary-amd64/Packages
 
 
-DOCKER_COMPOSE_VERSION="1.8.0"
-echo "\nUpdating Docker Compose to ${DOCKER_COMPOSE_VERSION}"
-sudo rm -f /usr/local/bin/docker-compose
-curl -L https://github.com/docker/compose/releases/download/${DOCKER_COMPOSE_VERSION}/docker-compose-`uname -s`-`uname -m` > docker-compose
-chmod +x docker-compose
-sudo mv docker-compose /usr/local/bin
+#DOCKER_COMPOSE_VERSION="1.8.0"
+#echo "\nUpdating Docker Compose to ${DOCKER_COMPOSE_VERSION}"
+#sudo rm -f /usr/local/bin/docker-compose
+#curl -L https://github.com/docker/compose/releases/download/${DOCKER_COMPOSE_VERSION}/docker-compose-`uname -s`-`uname -m` > docker-compose
+#chmod +x docker-compose
+#sudo mv docker-compose /usr/local/bin
