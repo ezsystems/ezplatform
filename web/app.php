9b03747bd2dae4193a779af24fdf36b89c17b5d8
André R <ar@ez.no>
2017-05-08 13:34:20 +0200

    Require PHP 7.0 and drop deprecated symfony boostrap code

--- web/app.php
+++ web/app.php
@@ -20,20 +20,12 @@ if (($useDebugging = getenv('SYMFONY_DEBUG')) === false || $useDebugging === '')
 
 /** @var \Composer\Autoload\ClassLoader $loader */
 $loader = require __DIR__ . '/../vendor/autoload.php';
-if (PHP_VERSION_ID < 70000 && !$useDebugging) {
-    include_once __DIR__ . '/../var/bootstrap.php.cache';
-}
 
 if ($useDebugging) {
     Debug::enable();
 }
 
 $kernel = new AppKernel($environment, $useDebugging);
-
-// we don't want to use the classes cache if we are in a debug session
-if (PHP_VERSION_ID < 70000 && !$useDebugging) {
-    $kernel->loadClassCache();
-}
 
 // Depending on the SYMFONY_HTTP_CACHE environment variable, tells whether the internal HTTP Cache mechanism is to be used.
 // If not set, or "", it is auto activated if _not_ in "dev" environment.
