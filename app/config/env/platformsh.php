2352494cae7e32aed28d91eac4c02bbadd28c3c0
SylvainGuittard <sylvain.guittard@gmail.com>
2017-06-16 09:17:29 -0400

    EZP-27499: udpated platform.sh hooks for v2.x

--- app/config/env/platformsh.php
+++ app/config/env/platformsh.php
@@ -20,6 +20,3 @@ foreach ($relationships['database'] as $endpoint) {
     $container->setParameter('database_password', $endpoint['password']);
     $container->setParameter('database_path', '');
 }
-
-// Store session into /tmp.
-ini_set('session.save_path', '/tmp/sessions');
