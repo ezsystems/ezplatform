69de1881467c4504c2b28e94f854a7d51c83a190
Kore Nordmann <mail@kore-nordmann.de>
2017-07-10 08:38:10 +0200

    Stubbed and registered templates

--- src/Kore/RatingFieldTypeBundle/DependencyInjection/KoreRatingFieldTypeExtension.php
+++ src/Kore/RatingFieldTypeBundle/DependencyInjection/KoreRatingFieldTypeExtension.php
@@ -5,15 +5,23 @@ namespace Kore\RatingFieldTypeBundle\DependencyInjection;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
+use Symfony\Component\DependencyInjection\Extension\PrependExtensionInterface;
 use Symfony\Component\DependencyInjection\Loader;
+use Symfony\Component\Yaml\Yaml;
 
 /**
  * This is the class that loads and manages your bundle configuration.
  *
  * @link http://symfony.com/doc/current/cookbook/bundles/extension.html
  */
-class KoreRatingFieldTypeExtension extends Extension
+class KoreRatingFieldTypeExtension extends Extension implements PrependExtensionInterface
 {
+    public function prepend(ContainerBuilder $container)
+    {
+        $config = Yaml::parse(file_get_contents(__DIR__ . '/../Resources/config/ezpublish.yml'));
+        $container->prependExtensionConfig('ezpublish', $config);
+    }
+
     /**
      * {@inheritdoc}
      */
