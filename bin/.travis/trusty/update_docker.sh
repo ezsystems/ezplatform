10be72815ce5b6a228378cdce642b773ce7203de
André R <andre.romcke@gmail.com>
2017-08-24 13:28:46 +0200

    EZP-27788: Update and cleanup docker config for Docker 17.06 features (#192)
    * [Docker] Update to Docker 17.06 & improve Mac performance with :cached
    
    Now that Docker 17.06 stable release is out, `:cached` and other features
    are now available in stable versions and can be used (17.04/5: edge).
    
    Bumps compose version all the way up to 3.3 which came in 17.06 for
    future use and hopefully not having to bump in in a while.
    
    * [Docker] Use healtcheck images for mariadb and redis
    
    Should theoretically get rid of the issue where app image starts up
    before mariadb is ready for connections, leading to user reaching 500 page.
    
    Note: The previous commit adding cached mounts for the sql file mostly fixes the
          issue, but this commit should in theory avoid app contianer starting to early.
    
    * [Docker] Add prod image for nginx
    
    Based on code from https://github.com/ezsystems/ezplatform-ee-demo/pull/33
    
    * [Docker] Update Varnish to use stretch & build config into image
    
    * [Travis] Use Docker Compose 1.14 and Docker 17.06
    
    * [Docker] Rename folder to doc/docker now that it contains more then just compose files
    
    As a bonus this also shorten the paths when working with the files.
    
    * [Docker] Swithc to dev by default now that we have :cached folders
    
    * [Doc] Update doc to better reflect what the bundled docker blueprints are for
    
    Aslo rather add links to Launchpad for people wanting to get up and running easily.
    
    * [Docker] Minor fixes in Dockerfiles
    
    * [Docker] Add env variable for malloc size

--- bin/.travis/trusty/update_docker.sh
+++ bin/.travis/trusty/update_docker.sh
@@ -3,7 +3,7 @@
 # Update package info and selectively update docker-engine (and keep old travis specific config file)
 docker -v
 sudo apt-get update
-sudo apt-get --reinstall -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" install docker-engine
+sudo apt-get --reinstall -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" install docker-ce
 docker -v
 
 # If we need to pin it to a given version:
@@ -12,7 +12,7 @@ docker -v
 
 
 docker-compose -v
-DOCKER_COMPOSE_VERSION="1.13.0"
+DOCKER_COMPOSE_VERSION="1.14.0"
 echo "\nUpdating Docker Compose to ${DOCKER_COMPOSE_VERSION}"
 sudo rm -f /usr/local/bin/docker-compose
 curl -L https://github.com/docker/compose/releases/download/${DOCKER_COMPOSE_VERSION}/docker-compose-`uname -s`-`uname -m` > docker-compose
