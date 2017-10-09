f7cb53c29a411ef4913c34197be4da3629d522ac
Nattfarinn <nattfarinn@gmail.com>
2017-10-09 17:17:16 +0200

    EZP-27888: Prepare Meta repository to use new eZ Platform Admin UI

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -5,16 +5,10 @@ use Symfony\Component\Config\Loader\LoaderInterface;
 
 class AppKernel extends Kernel
 {
-    /**
-     * Returns an array of bundles to registers.
-     *
-     * @return array an array of bundle instances
-     *
-     * @api
-     */
     public function registerBundles()
     {
         $bundles = [
+            // Symfony
             new Symfony\Bundle\FrameworkBundle\FrameworkBundle(),
             new Symfony\Bundle\SecurityBundle\SecurityBundle(),
             new Symfony\Bundle\TwigBundle\TwigBundle(),
@@ -23,45 +17,40 @@ class AppKernel extends Kernel
             new Symfony\Bundle\AsseticBundle\AsseticBundle(),
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
+            // Dependencies
             new Hautelook\TemplatedUriBundle\HautelookTemplatedUriBundle(),
             new Liip\ImagineBundle\LiipImagineBundle(),
             new FOS\HttpCacheBundle\FOSHttpCacheBundle(),
+            new Nelmio\CorsBundle\NelmioCorsBundle(),
+            new Oneup\FlysystemBundle\OneupFlysystemBundle(),
+            new JMS\TranslationBundle\JMSTranslationBundle(),
+            new Knp\Bundle\MenuBundle\KnpMenuBundle(),
+            // eZ Systems
             new EzSystems\PlatformHttpCacheBundle\EzSystemsPlatformHttpCacheBundle(),
             new eZ\Bundle\EzPublishCoreBundle\EzPublishCoreBundle(),
             new eZ\Bundle\EzPublishLegacySearchEngineBundle\EzPublishLegacySearchEngineBundle(),
             new eZ\Bundle\EzPublishIOBundle\EzPublishIOBundle(),
             new eZ\Bundle\EzPublishRestBundle\EzPublishRestBundle(),
-            new EzSystems\PlatformUIAssetsBundle\EzSystemsPlatformUIAssetsBundle(),
-            new EzSystems\PlatformUIBundle\EzSystemsPlatformUIBundle(),
             new EzSystems\EzSupportToolsBundle\EzSystemsEzSupportToolsBundle(),
-            new Nelmio\CorsBundle\NelmioCorsBundle(),
-            new WhiteOctober\PagerfantaBundle\WhiteOctoberPagerfantaBundle(),
-            new Oneup\FlysystemBundle\OneupFlysystemBundle(),
             new EzSystems\PlatformInstallerBundle\EzSystemsPlatformInstallerBundle(),
             new EzSystems\RepositoryFormsBundle\EzSystemsRepositoryFormsBundle(),
             new EzSystems\EzPlatformSolrSearchEngineBundle\EzSystemsEzPlatformSolrSearchEngineBundle(),
-            new EzSystems\EzPlatformDesignEngineBundle\EzPlatformDesignEngineBundle(),
-            new Bazinga\Bundle\JsTranslationBundle\BazingaJsTranslationBundle(),
-            new JMS\TranslationBundle\JMSTranslationBundle(),
-            new EzSystems\HybridPlatformUiBundle\EzSystemsHybridPlatformUiBundle(),
-            new FOS\JsRoutingBundle\FOSJsRoutingBundle(),
+            // new EzSystems\EzPlatformDesignEngineBundle\EzPlatformDesignEngineBundle(),
+            // Application
+            new EzSystems\EzPlatformAdminUiBundle\EzPlatformAdminUiBundle(),
             new AppBundle\AppBundle(),
         ];
 
-        switch ($this->getEnvironment()) {
-            case 'test':
-            case 'behat':
-                $bundles[] = new EzSystems\BehatBundle\EzSystemsBehatBundle();
-                $bundles[] = new EzSystems\PlatformBehatBundle\EzPlatformBehatBundle();
-                // No break, test also needs dev bundles
-            case 'dev':
-                $bundles[] = new eZ\Bundle\EzPublishDebugBundle\EzPublishDebugBundle();
-                $bundles[] = new Symfony\Bundle\DebugBundle\DebugBundle();
-                $bundles[] = new Symfony\Bundle\WebProfilerBundle\WebProfilerBundle();
-                $bundles[] = new Sensio\Bundle\DistributionBundle\SensioDistributionBundle();
+        if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
+            $bundles[] = new Symfony\Bundle\DebugBundle\DebugBundle();
+            $bundles[] = new Symfony\Bundle\WebProfilerBundle\WebProfilerBundle();
+            $bundles[] = new Sensio\Bundle\DistributionBundle\SensioDistributionBundle();
+
+            if ('dev' === $this->getEnvironment()) {
                 $bundles[] = new Sensio\Bundle\GeneratorBundle\SensioGeneratorBundle();
                 $bundles[] = new Symfony\Bundle\WebServerBundle\WebServerBundle();
-                $bundles[] = new Egulias\ListenersDebugCommandBundle\EguliasListenersDebugCommandBundle();
+                $bundles[] = new eZ\Bundle\EzPublishDebugBundle\EzPublishDebugBundle();
+            }
         }
 
         return $bundles;
@@ -74,30 +63,14 @@ class AppKernel extends Kernel
 
     public function getCacheDir()
     {
-        if (!empty($_SERVER['SYMFONY_TMP_DIR'])) {
-            return dirname($_SERVER['SYMFONY_TMP_DIR']) . '/var/cache/' . $this->getEnvironment();
-        }
-
         return dirname(__DIR__) . '/var/cache/' . $this->getEnvironment();
     }
 
     public function getLogDir()
     {
-        if (!empty($_SERVER['SYMFONY_TMP_DIR'])) {
-            return dirname($_SERVER['SYMFONY_TMP_DIR']) . '/var/logs';
-        }
-
         return dirname(__DIR__) . '/var/logs';
     }
 
-    /**
-     * Loads the container configuration.
-     *
-     * @param LoaderInterface $loader A LoaderInterface instance
-     * @throws \RuntimeException when config file is not readable
-     *
-     * @api
-     */
     public function registerContainerConfiguration(LoaderInterface $loader)
     {
         $loader->load($this->getRootDir() . '/config/config_' . $this->getEnvironment() . '.yml');
