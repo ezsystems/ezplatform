d51abedea742a54b299627f7d97c89f36cfcb8e8
Nattfarinn <nattfarinn@gmail.com>
2017-10-09 17:17:16 +0200

    EZP-27888: Prepare Meta repository to use new eZ Platform Admin UI

--- web/app.php
+++ web/app.php
@@ -1,57 +1,22 @@
 <?php
 
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\Debug\Debug;
 
-// Ensure UTF-8 is used in string operations
-setlocale(LC_CTYPE, 'C.UTF-8');
-
-// Environment is taken from "SYMFONY_ENV" variable, if not set, defaults to "prod"
-$environment = getenv('SYMFONY_ENV');
-if ($environment === false) {
-    $environment = 'prod';
+require __DIR__.'/../vendor/autoload.php';
+if (PHP_VERSION_ID < 70000) {
+    include_once __DIR__.'/../var/bootstrap.php.cache';
 }
 
-// Depending on the SYMFONY_DEBUG environment variable, tells whether Symfony should be loaded with debugging.
-// If not set, or "", it is auto activated if in "dev" environment.
-if (($useDebugging = getenv('SYMFONY_DEBUG')) === false || $useDebugging === '') {
-    $useDebugging = $environment === 'dev';
+$kernel = new AppKernel('prod', false);
+if (PHP_VERSION_ID < 70000) {
+    $kernel->loadClassCache();
 }
+//$kernel = new AppCache($kernel);
 
-/** @var \Composer\Autoload\ClassLoader $loader */
-$loader = require __DIR__ . '/../vendor/autoload.php';
-
-if ($useDebugging) {
-    Debug::enable();
-}
-
-$kernel = new AppKernel($environment, $useDebugging);
-
-// Depending on the SYMFONY_HTTP_CACHE environment variable, tells whether the internal HTTP Cache mechanism is to be used.
-// If not set, or "", it is auto activated if _not_ in "dev" environment.
-if (($useHttpCache = getenv('SYMFONY_HTTP_CACHE')) === false || $useHttpCache === '') {
-    $useHttpCache = $environment !== 'dev';
-}
-
-// Load HTTP Cache ...
-if ($useHttpCache) {
-    // The standard HttpCache implementation can be overridden by setting the SYMFONY_HTTP_CACHE_CLASS environment variable.
-    // NOTE: Make sure to setup composer config so it is *autoloadable*, or use "SYMFONY_CLASSLOADER_FILE" for this.
-    if ($httpCacheClass = getenv('SYMFONY_HTTP_CACHE_CLASS')) {
-        $kernel = new $httpCacheClass($kernel);
-    } else {
-        $kernel = new AppCache($kernel);
-    }
-}
+// When using the HttpCache, you need to call the method in your front controller instead of relying on the configuration parameter
+Request::enableHttpMethodParameterOverride();
 
 $request = Request::createFromGlobals();
-
-// If you are behind one or more trusted reverse proxies, you might want to set them in SYMFONY_TRUSTED_PROXIES environment
-// variable in order to get correct client IP
-if ($trustedProxies = getenv('SYMFONY_TRUSTED_PROXIES')) {
-    Request::setTrustedProxies(explode(',', $trustedProxies));
-}
-
 $response = $kernel->handle($request);
 $response->send();
 $kernel->terminate($request, $response);
