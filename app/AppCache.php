fa6a0231614fb312c574c4884ece03b925da1d3e
André R <ar@ez.no>
2017-10-13 18:07:31 +0200

    [AppKernel] Use native Symfony kernel and drop unneeded copyright headers (files are meant to be modified)

--- app/AppCache.php
+++ app/AppCache.php
@@ -1,11 +1,5 @@
 <?php
-/**
- * File containing the AppCache class.
- *
- * @copyright Copyright (C) eZ Systems AS. All rights reserved.
- * @license For full copyright and license information view LICENSE file distributed with this source code.
- * @version //autogentag//
- */
+
 use EzSystems\PlatformHttpCacheBundle\AppCache as PlatformHttpCacheBundleAppCache;
 
 /**
