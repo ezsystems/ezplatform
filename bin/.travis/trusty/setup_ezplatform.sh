0f162b9720b218505441796a555af59be8618ebd
Andrew Longosz <andrzej.longosz@ez.no>
2017-12-08 11:33:36 +0100

    [Travis] Optimized dependency setup for behat test
    Instead of using composer pointing to local checkout, it installs all
    dependencies and then overwrites the one being tested
    
    It's less time consuming, because composer doesn't try to read all
    tags and branches to find matching dependency before realizing
    the one its looking for truly exists in the local checkout :)

--- bin/.travis/trusty/setup_ezplatform.sh
+++ bin/.travis/trusty/setup_ezplatform.sh
@@ -31,40 +31,18 @@ fi
 if [[ -n "${DEPENDENCY_PACKAGE_DIR}" ]]; then
     # Get details about dependency package
     DEPENDENCY_PACKAGE_NAME=`php -r "echo json_decode(file_get_contents('${DEPENDENCY_PACKAGE_DIR}/composer.json'))->name;"`
-    DEPENDENCY_BRANCH_ALIAS=`php -r "echo json_decode(file_get_contents('${DEPENDENCY_PACKAGE_DIR}/composer.json'))->extra->{'branch-alias'}->{'dev-master'};"`
 
     if [[ -z "${DEPENDENCY_PACKAGE_NAME}" ]]; then
         echo 'Missing composer package name of tested dependency' >&2
         exit 3
     fi
-
-    if [[ -z "${DEPENDENCY_BRANCH_ALIAS}" ]]; then
-        echo 'Missing composer branch alias of tested dependency' >&2
-        exit 4
-    fi
-
-    # Use package name as directory name for better readability
-    TESTED_DEPENDENCY_DIR=`basename ${DEPENDENCY_PACKAGE_NAME}`
-    # Move package into location accessible by docker container
-    echo "> Move '${DEPENDENCY_PACKAGE_DIR}' to '${TESTED_DEPENDENCY_DIR}'"
-    mv ${DEPENDENCY_PACKAGE_DIR} ${TESTED_DEPENDENCY_DIR}
-
-    echo "> Modify eZ Platform composer.json to point to local checkout of ${DEPENDENCY_PACKAGE_NAME}"
-    composer config repositories.tested_dependency path ${TESTED_DEPENDENCY_DIR}
-
-    # require dependency using branch-alias version so local checkout gets symlinked
-    COMPOSER_REQUIRE="${DEPENDENCY_PACKAGE_NAME}:${DEPENDENCY_BRANCH_ALIAS}"
-    echo "> Requiring packages: $COMPOSER_REQUIRE"
-    composer require --no-update "${COMPOSER_REQUIRE}"
 fi
 
 echo '> Preparing eZ Platform container using the following setup:'
 echo "- EZPLATFORM_BUILD_DIR=${EZPLATFORM_BUILD_DIR}"
 echo "- COMPOSE_FILE=${COMPOSE_FILE}"
-if [[ -n "${DEPENDENCY_PACKAGE_DIR}" ]]; then
+if [[ -n "${DEPENDENCY_PACKAGE_NAME}" ]]; then
     echo "- DEPENDENCY_PACKAGE_NAME=${DEPENDENCY_PACKAGE_NAME}"
-    echo "- DEPENDENCY_PACKAGE_DIR=${DEPENDENCY_PACKAGE_DIR}"
-    echo "- DEPENDENCY_BRANCH_ALIAS=${DEPENDENCY_BRANCH_ALIAS}"
 fi
 
 echo '> Remove XDebug PHP extension'
@@ -76,6 +54,21 @@ docker-compose exec app sh -c 'chown -R www-data:www-data /var/www'
 
 echo '> Run composer install inside docker app container'
 docker-compose exec --user www-data app sh -c 'COMPOSER_HOME=$HOME/.composer composer install --no-suggest --no-progress --no-interaction --prefer-dist --optimize-autoloader'
+
+# Handle dependency if needed
+if [[ -n "${DEPENDENCY_PACKAGE_NAME}" ]]; then
+    # check if dependency exists for current meta-package version
+    if [[ ! -d "./vendor/${DEPENDENCY_PACKAGE_NAME}" ]]; then
+        echo "Testing dependency failed: package ${DEPENDENCY_PACKAGE_NAME} does not exist" >&2
+        exit 4
+    fi
+
+    echo "> Overwrite ./vendor/${DEPENDENCY_PACKAGE_NAME} with ${DEPENDENCY_PACKAGE_DIR}"
+    rm -rf "./vendor/${DEPENDENCY_PACKAGE_NAME}" && mv ${DEPENDENCY_PACKAGE_DIR} "./vendor/${DEPENDENCY_PACKAGE_NAME}"
+
+    echo '> Clear Symfony cache inside docker app container'
+    docker-compose exec --user www-data app sh -c 'php ./bin/console cache:clear --no-warmup && php ./bin/console cache:warmup'
+fi
 
 echo '> Install data'
 docker-compose exec --user www-data app sh -c "php /scripts/wait_for_db.php; php bin/console ezplatform:install ${INSTALL_TYPE}"
