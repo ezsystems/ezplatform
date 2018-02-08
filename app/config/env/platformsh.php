54f531a96809bb0396fad9723a785165a931de1f
Vidar Langseid <vl@ez.no>
2018-02-08 13:14:28 +0100

    Added DFS support on platform.sh

--- app/config/env/platformsh.php
+++ app/config/env/platformsh.php
@@ -18,12 +18,43 @@ foreach ($relationships['database'] as $endpoint) {
     $container->setParameter('database_name', $endpoint['path']);
     $container->setParameter('database_user', $endpoint['username']);
     $container->setParameter('database_password', $endpoint['password']);
-    $container->setParameter('database_path', '');
 
+    // 'cluster_database_name' is deprecated in eZ Platform 1.13.1/2.1
     // Cluster DB name is hardcoded. It will have no any effect if cluster is disabled
     $container->setParameter('cluster_database_name', 'cluster');
 }
 
+// PLATFORMSH_DFS_NFS_PATH is different compared to DFS_NFS_PATH in the sense that it is relative to ezplatform dir
+// DFS_NFS_PATH is an absolute path
+if ($dfsNfsPath = getenv('PLATFORMSH_DFS_NFS_PATH')) {
+    $container->setParameter('dfs_nfs_path', sprintf('%s/%s', getcwd(), $dfsNfsPath));
+
+    if (array_key_exists('dfs_database', $relationships)) {
+        foreach ($relationships['dfs_database'] as $endpoint) {
+            if (empty($endpoint['query']['is_master'])) {
+                continue;
+            }
+
+            $container->setParameter('dfs_database_driver', 'pdo_' . $endpoint['scheme']);
+            $container->setParameter('dfs_database_host', $endpoint['host']);
+            $container->setParameter('dfs_database_port', $endpoint['port']);
+            $container->setParameter('dfs_database_name', $endpoint['path']);
+            $container->setParameter('dfs_database_user', $endpoint['username']);
+            $container->setParameter('dfs_database_password', $endpoint['password']);
+        }
+    } else {
+        // If dfs_database endpoint is not defined, we'll use the default database for DFS too
+        $container->setParameter('dfs_database_driver', $container->getParameter('database_driver'));
+        $container->setParameter('dfs_database_host', $container->getParameter('database_host'));
+        $container->setParameter('dfs_database_port', $container->getParameter('database_port'));
+        $container->setParameter('dfs_database_name', $container->getParameter('database_name'));
+        $container->setParameter('dfs_database_user', $container->getParameter('database_user'));
+        $container->setParameter('dfs_database_password', $container->getParameter('database_password'));
+    }
+
+    $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__ . '/../dfs'));
+    $loader->load('dfs.yml');
+}
 // Use Redis-based caching if possible.
 if (isset($relationships['rediscache'])) {
     foreach ($relationships['rediscache'] as $endpoint) {
