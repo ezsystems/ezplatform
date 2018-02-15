f10701928eb854a5f6ca7195743b6817d8b8838f
André R <andre.romcke@gmail.com>
2018-02-15 14:14:25 +0100

    Cleanup/Improve Platform.sh config for 1.13 (and up) (#234)
    * Update platform.sh config to place cache outside disk & add more inline doc
    
    * Load cache tweaks only if redis and APCu is detected to be more errorprone
    
    * Add missing loading of memcached config to be consitent with redis
    
    * CS
    
    * For now remove cache tweaks for symfony as it might not be cleared on ca:cl
    
    * Remove solr config (will be handled seperatly) and other small cleanups
    
    * Fixed bug: Basedir of dfs_nfs_path was set incorrectly on Platform.sh in some cases
    
    * config comment out
    
    * Remove some inline notes which is not needed for now
    
    * Add deprecation for memcached and change comment on cache setting to avoid implying it will work out of the box
    
    * Add rm to clear compiled classes, container and such before running console

--- app/config/env/platformsh.php
+++ app/config/env/platformsh.php
@@ -30,7 +30,7 @@ foreach ($relationships['database'] as $endpoint) {
 // PLATFORMSH_DFS_NFS_PATH is different compared to DFS_NFS_PATH in the sense that it is relative to ezplatform dir
 // DFS_NFS_PATH is an absolute path
 if ($dfsNfsPath = getenv('PLATFORMSH_DFS_NFS_PATH')) {
-    $container->setParameter('dfs_nfs_path', sprintf('%s/%s', getcwd(), $dfsNfsPath));
+    $container->setParameter('dfs_nfs_path', sprintf('%s/%s', dirname($container->getParameter('kernel.root_dir')), $dfsNfsPath));
 
     if (array_key_exists('dfs_database', $relationships)) {
         foreach ($relationships['dfs_database'] as $endpoint) {
@@ -79,8 +79,13 @@ if (isset($relationships['rediscache'])) {
             continue;
         }
 
+        @trigger_error('Usage of Memcached is deprecated, redis is recommended', E_USER_DEPRECATED);
+
         $container->setParameter('cache_host', $endpoint['host']);
         $container->setParameter('cache_memcached_port', $endpoint['port']);
+
+        $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__ . '/../cache_pool'));
+        $loader->load('singlememcached.yml');
     }
 }
 
