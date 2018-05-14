4d619c8b2e79c14e5372b49e951f1f2d7539f547
Kamil Madejski <kamil.madejski@ez.no>
2018-05-14 11:34:13 +0200

    fixup! fixup! Enable Redis session storage

--- bin/platformsh/clear-cache.sh
+++ bin/platformsh/clear-cache.sh
@@ -2,8 +2,6 @@
 
 # Due to EZP-28816: console cache:clear doesn't remove cache from Redis, so we need to do it manually.
 
-. ../../.env
-
 function clearRedisCache
 {
     REDISHOST=`php -r '$r=getenv("PLATFORM_RELATIONSHIPS");$r=json_decode(base64_decode($r), true);echo($r["rediscache"][0]["host"]);'`
