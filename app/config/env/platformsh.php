d8400516e825d3d5022855d3a336c6e95b84ad9d
Vidar <vl@ez.no>
2018-08-09 09:06:45 +0200

    Added solr service template to platform.sh config (#282)

--- app/config/env/platformsh.php
+++ app/config/env/platformsh.php
@@ -119,3 +119,14 @@ if (isset($relationships['redissession'])) {
         $container->setParameter('session.save_path', sprintf('%s:%d', $endpoint['host'], $endpoint['port']));
     }
 }
+
+if (isset($relationships['solr'])) {
+    foreach ($relationships['solr'] as $endpoint) {
+        if ($endpoint['scheme'] !== 'solr') {
+            continue;
+        }
+
+        $container->setParameter('search_engine', 'solr');
+        $container->setParameter('solr_dsn', sprintf('http://%s:%d/%s', $endpoint['host'], $endpoint['port'], 'solr'));
+    }
+}
