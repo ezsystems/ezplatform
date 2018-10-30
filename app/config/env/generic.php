713095637dd7dea05e86f60b918bc3cc46d58e5a
André R <ar@ez.no>
2018-10-30 16:35:50 +0100

    [Docker] Add support for setting session.save_path via ENV

--- app/config/env/generic.php
+++ app/config/env/generic.php
@@ -71,3 +71,7 @@ if ($value = getenv('LOG_TYPE')) {
 if ($value = getenv('SESSION_HANDLER_ID')) {
     $container->setParameter('ezplatform.session.handler_id', $value);
 }
+
+if ($value = getenv('SESSION_SAVE_PATH')) {
+    $container->setParameter('ezplatform.session.save_path', $value);
+}
