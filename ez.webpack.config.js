71aee916404315fa01828cdefa38557da9359aec
Dariusz Szut <dew326@gmail.com>
2019-01-18 09:32:10 +0100

    changed externals

--- ez.webpack.config.js
+++ ez.webpack.config.js
@@ -9,48 +9,13 @@ module.exports = (Encore) => {
         Encore.setOutputPath('web/assets/build')
             .setPublicPath('/assets/build')
             .addExternals({
-                react: {
-                    root: 'React',
-                    commonjs2: 'react',
-                    commonjs: 'react',
-                    amd: 'react',
-                },
-                'react-dom': {
-                    root: 'ReactDOM',
-                    commonjs2: 'react-dom',
-                    commonjs: 'react-dom',
-                    amd: 'react-dom',
-                },
-                jquery: {
-                    root: 'jQuery',
-                    commonjs2: 'jquery',
-                    commonjs: 'jquery',
-                    amd: 'jquery',
-                },
-                moment: {
-                    root: 'moment',
-                    commonjs2: 'moment',
-                    commonjs: 'moment',
-                    amd: 'moment',
-                },
-                'popper.js': {
-                    root: 'Popper',
-                    commonjs2: 'popper',
-                    commonjs: 'popper',
-                    amd: 'popper',
-                },
-                alloyeditor: {
-                    root: 'AlloyEditor',
-                    commonjs2: 'AlloyEditor',
-                    commonjs: 'AlloyEditor',
-                    amd: 'AlloyEditor',
-                },
-                'prop-types': {
-                    root: 'PropTypes',
-                    commonjs2: 'prop-types',
-                    commonjs: 'prop-types',
-                    amd: 'prop-types',
-                }
+                react: 'React',
+                'react-dom': 'ReactDOM',
+                jquery: 'jQuery',
+                moment: 'moment',
+                'popper.js': 'Popper',
+                alloyeditor: 'AlloyEditor',
+                'prop-types': 'PropTypes',
             })
             .enableSassLoader()
             .enableReactPreset()
