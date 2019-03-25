594d1b07b7971debb349cd82b82c597e9b5c2b86
dew326 <dew326@gmail.com>
2019-03-25 16:25:53 +0100

    EZP-30345: Cannot override webpack encore JS/CSS from bundle (#378)

--- ez.webpack.config.js
+++ ez.webpack.config.js
@@ -1,5 +1,7 @@
 const path = require('path');
 const bundles = require('./var/encore/ez.config.js');
+const eZConfigManager = require('./ez.webpack.config.manager.js');
+const configManagers = require('./var/encore/ez.config.manager.js');
 
 module.exports = (Encore) => {
     Encore.setOutputPath('web/assets/ezplatform/build')
@@ -26,6 +28,12 @@ module.exports = (Encore) => {
     const eZConfig = Encore.getWebpackConfig();
 
     eZConfig.name = 'ezplatform';
+
+    configManagers.forEach((configManagerPath) => {
+        const configManager = require(configManagerPath);
+
+        configManager(eZConfig, eZConfigManager);
+    });
 
     return eZConfig;
 };
