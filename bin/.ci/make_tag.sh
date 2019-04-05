a358234c1b108283bb22d3e37fdca12473b48180
André R <ar@ez.no>
2019-04-05 12:22:56 +0200

    Make tag command

---
+++ bin/.ci/make_tag.sh
@@ -0,0 +1,68 @@
+#!/bin/bash
+# Script used to make eZ Platform/Enterprise meta tags
+#
+# Only aims to:
+# - prepare code for project use
+# - check some things to validate if we are ready for making tag
+# - make the tag
+#
+# Arguments:
+# - tag: v3.4.2
+# - composer args: arguments to pass to composer update
+
+set -e
+
+TAG=$1
+if [[ $TAG =~ ^v[0-9]+(\.[0-9]+){2,3}$ ]]; then
+  echo -e "\033[36m Start work on making tag $TAG \033[0m"
+else
+  echo -e "\033[31m Tag argument did not look correct, should be v1.2.33 \033[0m"
+  exit
+fi
+
+shift
+COMPOSER_ARGS="$@"
+CURRENT_BRANCH=`git branch | grep '*' | cut -d ' ' -f 2`
+
+# TODO: Add help text, display help on errors, check if checkout is unclean before starting so we don't throw them away
+
+
+# After this we want to be able to cleanup if things on exit (clean and error)
+clean_up () {
+    ARG=$?
+    git reset -q --hard HEAD
+    git checkout -q $CURRENT_BRANCH
+    git branch -q -D "tmp_release_$TAG"
+    exit $ARG
+}
+trap clean_up EXIT
+
+
+
+# Let's start!
+
+git checkout -b "tmp_release_$TAG"
+
+echo -e "\033[36m Comment out *.lock files in .gitignore \033[0m"
+perl -pi -e 's/^(.*).lock$/#$1.lock/g' .gitignore
+
+echo -e "\033[36m Set minimum php version in composer.json (temporary to get vendor cable of working with it) \033[0m"
+minimumPHP=$(php -r '$hash = json_decode(file_get_contents("composer.json"), true); $php = str_replace(["^", "~"], "", $hash["require"]["php"]); echo explode("|", $php)[0];')
+composer config platform.php "$minimumPHP"
+
+# TODO: Check that ez packages (vendor whitelist?) don't use @dev
+
+echo -e "\033[36m Update composer packages to generate lock files \033[0m"
+php -d memory_limit=-1 `which composer` update --no-interaction --prefer-dist $COMPOSER_ARGS
+
+echo -e "\033[36m Revert composer.json minimum php version changes, and update composer lock file hash to avoid warning \033[0m"
+git checkout composer.json
+php -d memory_limit=-1 `which composer` update --lock --no-scripts --no-interaction --prefer-dist $COMPOSER_ARGS
+
+echo -e "\033[36m Add changes, commit and tag \033[0m"
+git add -f *.lock .gitignore
+git commit -m "Configure $TAG for release"
+git tag -f $TAG
+
+echo -e "\033[36m \nReady to push the tag once you have checked it to be correct, once ready:\033[0m"
+echo -e "\033[33m git push <remote> $TAG\n \033[0m"
