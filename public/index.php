209ffd544e2544abb33bd7137d79eccf562d0e5a
Maciej Kobus <maciej.kobus@ez.no>
2019-06-04 13:59:44 +0200

    [WORKAROUND] Temporary disabled CacheKernel

--- public/index.php
+++ public/index.php
@@ -44,7 +44,8 @@ if (($useHttpCache = getenv('APP_HTTP_CACHE')) === false || $useHttpCache === ''
 
 // Load internal HTTP Cache, aka Symfony Proxy, if enabled
 if ($useHttpCache) {
-    $kernel = new CacheKernel($kernel);
+    // @todo uncomment once HttpCache is working back again
+//    $kernel = new CacheKernel($kernel);
 
     // Needed when using Synfony proxy, see: http://symfony.com/doc/3.4/reference/configuration/framework.html#http-method-override
     Request::enableHttpMethodParameterOverride();
