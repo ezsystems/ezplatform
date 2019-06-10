1f1625eb1303ec6c0ed142ca7418f1e0d8201c3e
Maciej Kobus <maciej.kobus@ez.no>
2019-06-10 15:35:52 +0200

    Added minor fixes after code reviews

--- src/CacheKernel.php
+++ src/CacheKernel.php
@@ -5,9 +5,7 @@ namespace App;
 use EzSystems\PlatformHttpezCacheBundle\AppCache as PlatformHttpCacheBundleAppCache;
 
 /**
- * Class AppCache.
- *
- * For easier upgrade do not change this file, prefer to adapt web/app.php instead.
+ * For easier upgrade do not change this file, prefer to adapt public/index.php instead.
  */
 class CacheKernel extends PlatformHttpCacheBundleAppCache
 {
