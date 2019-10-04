f576f040aad166eee8288ec7c1397103c4e9ab7f
André R <ar@ez.no>
2019-10-04 11:07:16 +0200

    [platformsh_prestart_cacheclear.sh] Set default for SYMFONY_ENV variable

--- bin/platformsh_prestart_cacheclear.sh
+++ bin/platformsh_prestart_cacheclear.sh
@@ -5,8 +5,8 @@
 set -e
 
 #date
-echo "removing var/cache/${SYMFONY_ENV}/*.* to avoid Symfony container issues on interface changes"
-rm -Rf var/cache/${SYMFONY_ENV}/*.*
+echo "removing var/cache/${SYMFONY_ENV-dev}/*.* to avoid Symfony container issues on interface changes"
+rm -Rf var/cache/${SYMFONY_ENV-dev}/*.*
 #date
 echo "clearing application cache"
 php bin/console cache:clear
