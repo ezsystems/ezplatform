c0b579d238d6e73b85c08ef8b5c7f837cfdd593d
Marek Nocoń <mnocon@users.noreply.github.com>
2020-06-09 15:35:42 +0200

    [TMP] Add a bit od debug

--- bin/.travis/trusty/update_docker.sh
+++ bin/.travis/trusty/update_docker.sh
@@ -33,12 +33,14 @@ GITHUB_TOKEN=$(cat ${SCRIPT_DIR}/../composer-auth.json | jq -r '.["github-oauth"
 PLATFORM=docker-compose-`uname -s`-`uname -m`
 
 DOCKER_COMPOSE_DOWNLOAD_URL=$(curl -H "Authorization: token ${GITHUB_TOKEN}" https://api.github.com/repos/docker/compose/releases/tags/${DOCKER_COMPOSE_VERSION} | jq -r --arg p "$PLATFORM" '.assets[] | select(.name == $p) | .url')
+echo "Downloading docker-compose from ${DOCKER_COMPOSE_DOWNLOAD_URL}"
 curl -L -H "Authorization: token ${GITHUB_TOKEN}" -H "Accept: application/octet-stream" $DOCKER_COMPOSE_DOWNLOAD_URL > docker-compose-dl
 
 FILE_TYPE=$(file -b --mime-type docker-compose-dl | sed 's|/.*||')
 if [[ $FILE_TYPE == "application" ]]; then
     sudo rm -f /usr/local/bin/docker-compose
     chmod +x docker-compose-dl
+    cat docker-compose-dl
     sudo mv docker-compose-dl /usr/local/bin/docker-compose
 else
     echo "Error when downloading docker-compose"
