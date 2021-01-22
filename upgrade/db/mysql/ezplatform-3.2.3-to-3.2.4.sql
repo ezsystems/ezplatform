9ca62a9ce02c6ace93c50205e7586affeeb1f52d
Tomasz Kryszan <tomasz.kryszan@ez.no>
2021-01-22 11:27:10 +0100

    EZEE-3453: Added SQL update scripts

---
+++ upgrade/db/mysql/ezplatform-3.2.3-to-3.2.4.sql
@@ -0,0 +1,6 @@
+UPDATE ezuser SET email =
+    CASE
+        WHEN contentobject_id = 10 THEN 'anonymous@link.invalid'
+        WHEN contentobject_id = 14 THEN 'admin@link.invalid'
+    END
+WHERE contentobject_id IN (10, 14);
